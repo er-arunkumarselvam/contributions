8ae4f9807b195d096f690bd82ed5da1b265b0b98
er-arunkumarselvam <er.arunkumar_selvam@outlook.com>
2022-10-13 20:07:50 +0530

    My understanding of Constructors in java

---
+++ Example1.java
@@ -0,0 +1,12 @@
+//Example for Default Constructor - The Program will Run Successfull.
+//Constructor is a special type of method which is used to initialize the object.        
+//Default Constructor is invisible or Implicitly. It is taken out. It is being overloaded and called as parameterized constructor
+class Example{
+public static void main(String[] args)
+{
+Example name= new Example();
+}
+
+}
+
+
